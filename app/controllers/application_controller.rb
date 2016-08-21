0b2321bd9f4dc2f34b7043989528962240692729
M A Manazir Ahsan <manazirahsan.developer@gmail.com>
2016-08-21 14:31:10 +0800

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def greeting
-  	render text: "<html><center>Bismillahir Rahmanir Rahim<br>Call me, Manna!!</center></html>"
-  end
 end
